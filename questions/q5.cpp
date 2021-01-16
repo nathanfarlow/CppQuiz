779bd3103a00093fb9bc2bd32ba22e251c1007e1
Nathan Farlow <natethegreatest11@gmail.com>
2021-01-16 00:41:30 -0600

    Add q5

---
+++ questions/q5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class Object {
+public:
+    int a_, b_, c_;
+
+    Object(int val) : c_(val), b_(2 * c_), a_(2 * b_) {}
+};
+
+auto main() -> int {
+    Object o(2);
+    std::cout << o.a_ << std::endl;
+    return 0;
+}
